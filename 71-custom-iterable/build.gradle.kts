9f32980e7ccffb981a6a23a0fcdbb3c58caab0f2
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-11-04 18:44:43 +0100

    changes to build.gradle.kts

--- 71-custom-iterable/build.gradle.kts
+++ 71-custom-iterable/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     java
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+    /*
+     * Adds tasks to export a runnable jar.
+     * Always pick the latest version!
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "7.0.0" // Update the version!
 }
