3bc2399be63ee45e3689d53d26b5e9ce69ebc1da
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-11-04 18:42:59 +0100

    implementation of MonthSorter into MonthSorterNested, now pass all test

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -11,11 +11,72 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDays();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
+    }
+
+    private enum Month {
+
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(final int days) {
+            this.days = days;
+        }
+
+        private static Month fromString(final String name) {
+            Objects.requireNonNull(name);
+            try {
+                return valueOf(name);
+            } catch (IllegalArgumentException e) {
+                // Fallback to manual scan before giving up
+                Month match = null;
+                for (final Month month: values()) {
+                    if (month.toString().toLowerCase(Locale.ROOT).startsWith(name.toLowerCase(Locale.ROOT))) {
+                        if (match != null) {
+                            throw new IllegalArgumentException(
+                                    name + " is ambiguous: both " + match + " and " + month + " would be valid matches", e
+                            );
+                        }
+                        match = month;
+                    }
+                }
+                if (match == null) {
+                    throw new IllegalArgumentException("No matching months for " + name, e);
+                }
+                return match;
+            }
+        }
+
+    }
+
+    private static final class SortByDays implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return  Integer.compare(Month.fromString(o1).days, Month.fromString(o2).days);
+        }
+    }
+
+    private static final class SortByMonthOrder implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return Month.fromString(o1).compareTo(Month.fromString(o2));
+        }
     }
 }
